5a57f4e01e9c0a740e0dc72d89c56ff24888028c
AlexKih <eug-i@yandex.ru>
2023-08-18 21:15:31 +0300

    Занятие 10, задание

--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from .models import Advertisement
 from .forms import AdvertisementForm
@@ -15,6 +15,15 @@ def register(request):
     return render(request, 'register.html')
 
 def advertisement_post(request):
-    form = AdvertisementForm()
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement=Advertisement(**form.cleaned_data)
+            advertisement.user=request.user
+            advertisement.save()
+            url = reverse('main-page')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
     context = {'form':form}
     return render(request, 'advertisement-post.html', context)
